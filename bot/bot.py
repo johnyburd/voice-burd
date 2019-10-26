dc7b0d3ef965473a2b4b13d875ff6a28f55b4aec
Jonathan Buchanan <johnyburd@gatech.edu>
2019-10-25 21:52:40 -0400

    first

---
+++ bot/bot.py
@@ -0,0 +1,20 @@
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+client = MyClient()
+
+
+with open(".token") as f:
+    print(f.read().strip())
+    client.run(f.read().strip())
